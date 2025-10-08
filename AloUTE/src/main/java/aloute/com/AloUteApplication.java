a45b42ae758431c74ff37b3fd077cdf9fa81164b
Phạm Lê Anh Tuấn <phamleanhtuan16022005@gmail.com>
2025-10-08 13:26:55 +0700

    Tái cấu trúc

---
+++ AloUTE/src/main/java/aloute/com/AloUteApplication.java
@@ -0,0 +1,13 @@
+package aloute.com;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AloUteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AloUteApplication.class, args);
+	}
+
+}
--- AloUTE/src/main/java/aloutesocial/com/service/EmailService.java
+++
@@ -1,7 +0,0 @@
-package aloutesocial.com.service;
-
-public interface EmailService {
-	public void sendEmail(String toEmail, String subject, String body);
-	public String generateOTP(int length);
-	public String getEmail(String username);
-}
