deaf5d3917144e16045477f3a92194bee4b36157
Phạm Lê Anh Tuấn <phamleanhtuan16022005@gmail.com>
2025-10-27 14:12:17 +0700

    Fix

--- UTESocial/src/main/java/aloute/com/repository/common/PostsRepository.java
+++ UTESocial/src/main/java/aloute/com/repository/common/PostsRepository.java
@@ -199,31 +199,32 @@ public interface PostsRepository extends JpaRepository<Posts, Integer> {
         return findPostsWithFiltersForAdmin(keyword, status, startDate, endDate, startDateTime, endDateTime, pageable); // Truyền Pageable
     }
 
-
-	//Lấy danh sách Post và lọc post
-	@Query("SELECT DISTINCT p FROM Posts p LEFT JOIN FETCH p.user u WHERE p.isDeleted = false " +
-			"AND (:keyword IS NULL OR :keyword = '' OR p.content LIKE LOWER(CONCAT('%', :keyword, '%')) OR LOWER(u.fullName) LIKE LOWER(CONCAT('%', :keyword, '%')) OR LOWER(u.nameUser) LIKE LOWER(CONCAT('%', :keyword, '%'))) " +
-			"AND (:status IS NULL OR :status = '' OR p.status = :status) " +
-			
-			"AND (CAST(:startDate AS date) IS NULL OR p.createdAt >= :startDateTime) " + // So sánh với đầu ngày startDate
-			"AND (CAST(:endDate AS date) IS NULL OR p.createdAt <= :endDateTime) "   // So sánh với cuối ngày endDate
-			) 
-		Page<Posts> findPostsWithFiltersForAdmin
-		(
-				@Param("keyword") String keyword,
-				@Param("status") String status,
-				
-				@Param("startDate") LocalDate startDate,
-				@Param("endDate") LocalDate endDate,
-				@Param("startDateTime") LocalDateTime startDateTime, 
-				@Param("endDateTime") LocalDateTime endDateTime,
-				Pageable pageable
-		);
-	default Page<Posts> findPostsWithFiltersForAdmin(String keyword, String status, LocalDate startDate, LocalDate endDate, Pageable pageable) 
-	{ 
-        LocalDateTime startDateTime = (startDate != null) ? startDate.atStartOfDay() : null;
-        LocalDateTime endDateTime = (endDate != null) ? endDate.atTime(LocalTime.MAX) : null;
-        return findPostsWithFiltersForAdmin(keyword, status, startDate, endDate, startDateTime, endDateTime, pageable); // Truyền Pageable
-    }
+	@Query("SELECT DISTINCT p FROM Posts p LEFT JOIN FETCH p.user WHERE p.isDeleted = false ORDER BY p.createdAt DESC")
+    List<Posts> findAllWithUserForAdmin();
+//	//Lấy danh sách Post và lọc post
+//	@Query("SELECT DISTINCT p FROM Posts p LEFT JOIN FETCH p.user u WHERE p.isDeleted = false " +
+//			"AND (:keyword IS NULL OR :keyword = '' OR p.content LIKE LOWER(CONCAT('%', :keyword, '%')) OR LOWER(u.fullName) LIKE LOWER(CONCAT('%', :keyword, '%')) OR LOWER(u.nameUser) LIKE LOWER(CONCAT('%', :keyword, '%'))) " +
+//			"AND (:status IS NULL OR :status = '' OR p.status = :status) " +
+//			
+//			"AND (CAST(:startDate AS date) IS NULL OR p.createdAt >= :startDateTime) " + // So sánh với đầu ngày startDate
+//			"AND (CAST(:endDate AS date) IS NULL OR p.createdAt <= :endDateTime) "   // So sánh với cuối ngày endDate
+//			) 
+//		Page<Posts> findPostsWithFiltersForAdmin
+//		(
+//				@Param("keyword") String keyword,
+//				@Param("status") String status,
+//				
+//				@Param("startDate") LocalDate startDate,
+//				@Param("endDate") LocalDate endDate,
+//				@Param("startDateTime") LocalDateTime startDateTime, 
+//				@Param("endDateTime") LocalDateTime endDateTime,
+//				Pageable pageable
+//		);
+//	default Page<Posts> findPostsWithFiltersForAdmin(String keyword, String status, LocalDate startDate, LocalDate endDate, Pageable pageable) 
+//	{ 
+//        LocalDateTime startDateTime = (startDate != null) ? startDate.atStartOfDay() : null;
+//        LocalDateTime endDateTime = (endDate != null) ? endDate.atTime(LocalTime.MAX) : null;
+//        return findPostsWithFiltersForAdmin(keyword, status, startDate, endDate, startDateTime, endDateTime, pageable); // Truyền Pageable
+//    }
 
 }
